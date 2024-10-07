642d0d2313f4c8f1ad91468ae98747734555a10e
Marcos Albano <marcos.telematica@gmail.com>
2024-10-07 17:44:23 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/main/java/com/idace/pontoidace/api/PontoIdaceApplication.java
@@ -0,0 +1,13 @@
+package com.idace.pontoidace.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoIdaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoIdaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idace/pontoidace/api/PontoIdaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idace.pontoidace.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoIdaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
